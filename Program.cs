7457ca778cd2244c800ba775511c2a10f8955a93
duy-vuong160102 <duyvuong1601@gmail.com>
2023-07-04 21:37:57 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_BAI1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten của ban: ");
+            String yourname = Console.ReadLine();
+            Console.WriteLine("hello:" +yourname);
+        }
+    }
+}
